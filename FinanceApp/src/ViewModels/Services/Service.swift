eb53e599b4a1d7c06e954d0e3af8aa369b8459bd
Joseph Bezerra <joseph.carlosx@gmail.com>
2024-11-08 12:01:22 -0400

    feat: create services to fetch

---
+++ FinanceApp/src/ViewModels/Services/Service.swift
@@ -0,0 +1,58 @@
+//
+//  Service.swift
+//  FinanceApp
+//
+//  Created by Joseph Pereira on 08/11/24.
+//
+
+import SwiftData
+
+struct Service<T: PersistentModel>: PCrud{
+    
+    typealias ModelType = T
+    private var modelContext: ModelContext? = nil
+    
+    func create(_ model: T) -> Bool {
+        modelContext?.insert(model)
+        do{
+            try modelContext?.save()
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    func read() -> [T] {
+        let descriptor = FetchDescriptor<T>()
+        do{
+            
+            let results = try modelContext?.fetch(descriptor) ?? []
+            return results
+        } catch {
+            return []
+        }
+    }
+    
+    func update(_ model: T, changes: (inout T) -> Void) -> Bool {
+        var modelToUpdate = model
+        
+        do{
+            try modelContext?.transaction {
+                changes(&modelToUpdate)
+            }
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    func delete(_ model: T) -> Bool {
+        do{
+            modelContext?.delete(model)
+            try modelContext?.save()
+            return true
+        } catch {
+            return false
+        }
+    }
+}
