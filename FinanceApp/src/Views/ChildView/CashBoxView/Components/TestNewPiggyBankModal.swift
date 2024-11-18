1798d824d2c697e190ba213e828795709ff30bb4
grecia <grecia.rivera@icomp.ufam.edu.br>
2024-11-18 14:36:54 -0400

    Aviso para piggybank n receber valor 0

--- FinanceApp/src/Views/ChildView/CashBoxView/Components/TestNewPiggyBankModal.swift
+++ FinanceApp/src/Views/ChildView/CashBoxView/Components/TestNewPiggyBankModal.swift
@@ -4,21 +4,27 @@
 //
 //  Created by Grecia Cristina on 08/11/24.
 //
-
 import SwiftUI
+
 struct TestNewPiggyBankModal: View {
     @Binding var isPresented: Bool
-//    @ObservedObject var viewModel: TestCashBoxViewModel
-    var goalToEdit: GoalBankModel? // `goalToEdit` agora é opcional
+    var goalToEdit: GoalBankModel?
     @State private var goalName = ""
     @State private var goalAmount = ""
     
     @State var addGoal: (_ name: String, _ amount: Int) -> Void
     
+   
+    private var isGoalAmountValid: Bool {
+        if let amount = Int(goalAmount), amount > 0 {
+            return true
+        }
+        return false
+    }
+    
     init(isPresented: Binding<Bool>, goalToEdit: GoalBankModel? = nil, addGoal: @escaping (_ name: String, _ amount: Int) -> Void) {
         self.addGoal = addGoal
         self._isPresented = isPresented
-//        self.viewModel = viewModel
         self.goalToEdit = goalToEdit
         _goalName = State(initialValue: goalToEdit?.cashBox.cashBoxDescription ?? "")
         _goalAmount = State(initialValue: "\(goalToEdit?.goalAmount ?? 0)")
@@ -30,32 +36,26 @@ struct TestNewPiggyBankModal: View {
                 Button("Cancel") {
                     isPresented = false
                 }
-                .font(Font.custom("Pally-Bold", size: 17)
-//                    .weight(.medium)
-                ).foregroundColor(.mediumPurple)
+                .font(Font.custom("Pally-Bold", size: 17))
+                .foregroundColor(.mediumPurple)
                 
                 Spacer()
                 
                 Text(goalToEdit == nil ? "New Piggy Bank" : "Edit Piggy Bank")
                     .foregroundColor(.primary)
                     .font(Font.custom("Pally-Bold", size: 17))
-//                        .weight(.medium))
                 
                 Spacer()
                 
                 Button("Done") {
                     if let amount = Int(goalAmount) {
-//                        if let goal = goalToEdit {
-//                            viewModel.updateGoal(goal: goal, name: goalName, amount: amount)
-//                        } else {
-//                            viewModel.addGoal(name: goalName, amount: amount)
-//                        }
                         addGoal(goalName, amount)
                         isPresented = false
                     }
                 }
-                .font(Font.custom("Pally-Bold", size: 17).weight(.medium)
-                ).foregroundColor(.mediumPurple)
+                .font(Font.custom("Pally-Bold", size: 17).weight(.medium))
+                .foregroundColor(.mediumPurple)
+                .disabled(!isGoalAmountValid) // Desativa o botão se o valor for inválido
             }
             .padding([.top, .horizontal])
             
@@ -63,8 +63,7 @@ struct TestNewPiggyBankModal: View {
             
             VStack(alignment: .leading, spacing: 8) {
                 Text("What do you want to buy?")
-                    .font(Font.custom("Pally-Bold", size: 17)
-                        .weight(.medium))
+                    .font(Font.custom("Pally-Bold", size: 17).weight(.medium))
                     .foregroundColor(Color.black.opacity(0.7))
                 
                 TextField("Enter item", text: $goalName)
@@ -84,16 +83,21 @@ struct TestNewPiggyBankModal: View {
                     .background(Color.white)
                     .cornerRadius(10)
                     .font(Font.custom("Pally-Bold", size: 17).weight(.medium))
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray.opacity(0.5)))
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(isGoalAmountValid ? Color.gray.opacity(0.5) : Color.red)) // Mostra vermelho se inválido
+                
+                if !isGoalAmountValid && !goalAmount.isEmpty {
+                    Text("Please enter a valid amount greater than 0.")
+                        .font(Font.custom("Pally-Regular", size: 14))
+                        .foregroundColor(.red)
+                        .padding(.top, 4)
+                }
             }
             .padding(.horizontal)
             
             Spacer()
         }
-//        .background(Color(red: 22, green: 22, blue: 22))
         .padding()
         .cornerRadius(20)
         .frame(width: 500, height: 300)
     }
-    
 }
